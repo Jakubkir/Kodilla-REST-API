c2fda5f79769492f4c94fd9de5713e4fead44a4b
Jakub Kirker <jakubkir@wp.pl>
2022-01-27 20:23:08 +0100

    19.2 wyszukiwanie pojedynczego zadania

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,33 +1,45 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
-    @GetMapping
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        return new TaskDto(1L, "Postman", "jest do kitu zadanie 19.2");
     }
 
     @DeleteMapping
     public void deleteTask(Long taskId) {
 
     }
+
     @PutMapping
     public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
+        return new TaskDto(2L, "Postman", "jest do kitu");
     }
+
     @PostMapping
     public void createTask(TaskDto taskDto) {
 
     }
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
 }
---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,42 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskMapper {
+
+    public Task mapToTask(final TaskDto taskDto) {
+        return new Task(
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getContent()
+        );
+    }
+
+    public TaskDto mapToTaskDto(final Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getContent()
+        );
+    }
+
+    public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
+        return taskList.stream()
+                .map(this::mapToTaskDto)
+                .collect(Collectors.toList());
+    }
+
+}
+
+
