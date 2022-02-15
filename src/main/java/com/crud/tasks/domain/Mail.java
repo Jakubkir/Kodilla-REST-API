6d71e4d211e776c2d20bab27d12d96bd62a35f61
Jakub Kirker <jakubkir@wp.pl>
2022-02-15 21:28:47 +0100

    23,3 Zadaniedodanie maila do wiadomości (ang. CC)

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mail {
+    private  String mailTo;
+    private  String subject;
+    private  String message;
+    private  String ToCC;
+}
